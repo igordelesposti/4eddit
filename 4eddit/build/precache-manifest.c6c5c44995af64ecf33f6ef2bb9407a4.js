7470233514f5d1088fa6366abc4867fe4e0cc434
Igor Delesposti <56797122+igordelesposti@users.noreply.github.com>
2020-07-27 15:27:55 -0300

    Entrega final 4eddit
    Finalização do layout.

---
+++ 4eddit/build/precache-manifest.c6c5c44995af64ecf33f6ef2bb9407a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1236012ec9f1129b1f0edaca62e0550",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fb95c4445999c9f48eb",
+    "url": "/static/js/2.a647fad4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a647fad4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6fb363318a76a8af943",
+    "url": "/static/js/main.60e963a9.chunk.js"
+  },
+  {
+    "revision": "6ebb3750e6873afc70de",
+    "url": "/static/js/runtime-main.d4c2ee40.js"
+  }
+]);
